669c2612485e6996871941c0fb167e7ca8eb4eb4
Euna789 <55429220+Euna789@users.noreply.github.com>
2020-08-13 17:15:26 +0900

    img title arrange

--- index/cardnews.js
+++ index/cardnews.js
@@ -4,13 +4,17 @@ console.log(imgBtn);
 function paintMenuImg(folderName){
     const li=document.createElement("li");
     li.className="menu";
+    const div=document.createElement("div");
+    div.className="imgTitle";
     const img=document.createElement("img");
+    img.className="img";
     img.setAttribute("src","../images/"+folderName+"/"+1+".png");
     img.setAttribute("width","300");
     const title=document.createTextNode(folderName);
     title.innerText=folderName;
-    li.appendChild(img);
-    li.appendChild(title);
+    div.appendChild(img);
+    div.appendChild(title);
+    li.appendChild(div);
     imgBtn.appendChild(li);
 }
 
